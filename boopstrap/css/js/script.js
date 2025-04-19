de5efde403b440071f5cfceef30b57d3173cfa74
Lara <lmiene716@gmail.com>
2025-04-18 21:17:29 -0300

    Delete boopstrap/css/js directory

--- boopstrap/css/js/script.js
+++
@@ -1,9 +0,0 @@
-function mostrarMensagem() {
-    const div = document.getElementById("mensagem");
-    div.textContent = "Seja bem-vinda à minha página pessoal!";
- 
-    // Esconde a mensagem após 5 segundos (5000 milissegundos)
-  setTimeout(() => {
-    div.textContent = "";
-  }, 5000);
-}
